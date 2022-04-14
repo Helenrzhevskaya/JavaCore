1f1582241732f656b55629c1f59215a0f88b8838
LenaR7 <helenrzhevskaya@mail.ru>
2022-04-14 20:12:28 +0500

    done hw3

---
+++ src/main/java/homework3/Fruit.java
@@ -0,0 +1,19 @@
+package homework3;
+
+public class Fruit {
+
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
